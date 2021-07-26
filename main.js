93bb7e404331b2e1f6ee36a6fb6a9a4bdde4bd7d
Shaifullah Arfan <shaifullaharfan17@gmail.com>
2021-07-27 04:40:29 +0600

    refactor & nav overlay click close

--- main.js
+++ main.js
@@ -3,15 +3,21 @@ const navList = document.querySelector('.nav__list');
 const navIconEl = document.querySelector('.nav__icon');
 const navBgOverlayEl = document.querySelector('.nav__bgOverlay');
 
-navIconEl.addEventListener('click', () => {
-  navList.classList.add('show');
+const navOpen = () => {
+   navList.classList.add('show');
   navBgOverlayEl.classList.add('active');
   document.body.style= 'height: 100vh; width:100vw; overflow:hidden;';
-});
+}
 
-navCloseEl.addEventListener('click', () => {
+const navClose = () => {
   navList.classList.remove('show');
   navBgOverlayEl.classList.remove('active');
   document.body.style= 'height: initial; width: 100%; overflow-x: hidden;';
-});
+}
+
+navIconEl.addEventListener('click', navOpen);
+
+navCloseEl.addEventListener('click', navClose);
+
+navBgOverlayEl.addEventListener('click', navClose)
 
