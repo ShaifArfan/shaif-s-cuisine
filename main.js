c03e997eb89924b7b7912b815014a169aaffd007
Shaifullah Arfan <shaifullaharfan17@gmail.com>
2021-07-27 04:26:32 +0600

    ✅ adding nav menu

---
+++ main.js
@@ -0,0 +1,17 @@
+const navCloseEl = document.querySelector('.nav__close');
+const navList = document.querySelector('.nav__list');
+const navIconEl = document.querySelector('.nav__icon');
+const navBgOverlayEl = document.querySelector('.nav__bgOverlay');
+
+navIconEl.addEventListener('click', () => {
+  navList.classList.add('show');
+  navBgOverlayEl.classList.add('active');
+  document.body.style= 'height: 100vh; overflow:hidden;';
+});
+
+navCloseEl.addEventListener('click', () => {
+  navList.classList.remove('show');
+  navBgOverlayEl.classList.remove('active');
+  document.body.style= 'height: initial; overflow: initial;';
+});
+
