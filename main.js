4d514be447991ce336f90f81546f9ae2ac0721c1
Shaifullah Arfan <shaifullaharfan17@gmail.com>
2021-07-30 20:30:30 +0600

    adding scroll animation and form required fields

--- main.js
+++ main.js
@@ -26,3 +26,22 @@ navCloseEl.addEventListener('click', navClose);
 
 navBgOverlayEl.addEventListener('click', navClose)
 
+// AOS
+// AOS.refreshHard();
+AOS.init({
+  offset: 200, // offset (in px) from the original trigger point
+  delay: 100, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
+
+// const dishGridEl = Array.from(document.querySelectorAll('#dishGrid'));
+// if (dishGridEl.length > 0){
+//   // console.log(dishGridEl)
+//   dishGridEl.forEach(item => {
+//     item.setAttribute('data-aos', 'fade-up');
+//   })
+// }
